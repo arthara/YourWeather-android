cd5e884749b8346ac35e674d855087c628c52698
AanMFA <aanmfa06@gmail.com>
2020-12-14 23:12:54 +0700

    Add testing for forecast and a little refactoring (#3)

--- app/src/main/java/com/kimi/tenki/TestActivity.java
+++ app/src/main/java/com/kimi/tenki/TestActivity.java
@@ -9,7 +9,8 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.kimi.tenki.current.CurrentModel;
 import com.kimi.tenki.current.CurrentService;
-import com.kimi.tenki.current.CurrentServiceGenerator;
+import com.kimi.tenki.forecast.ForecastService;
+import com.kimi.tenki.shared.retrofit.ServiceGenerator;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,7 +20,15 @@ public class TestActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CurrentService service = CurrentServiceGenerator.createService(CurrentService.class);
+        //test api
+        testForecastAPI();
+        //test api
+        //Intent intent = new Intent(this, MapsActivity.class);
+//        startActivity(intent);
+    }
+
+    private void testCurrentAPI(){
+        CurrentService service = ServiceGenerator.createService(CurrentService.class);
         Call call = service.getByLocation(80.02f, -7.82f);
         call.enqueue(new Callback<CurrentModel>() {
             @Override
@@ -31,13 +40,30 @@ public class TestActivity extends FragmentActivity {
                 }
             }
 
+            @Override
+            public void onFailure(Call<CurrentModel> call, Throwable t) {
+                Log.d("Request Failure", t.getMessage());
+            }
+        });
+    }
+
+    private void testForecastAPI(){
+        ForecastService service = ServiceGenerator.createService(ForecastService.class);
+        Call call = service.getByLocation(80.02f, -7.82f);
+        call.enqueue(new Callback<CurrentModel>() {
+            @Override
+            public void onResponse(Call<CurrentModel> call, Response<CurrentModel> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Log.d("Berhasil", response.body().toString());
+                } else {
+                    Log.d("Gagal", response.toString());
+                }
+            }
 
             @Override
             public void onFailure(Call<CurrentModel> call, Throwable t) {
                 Log.d("Request Failure", t.getMessage());
             }
         });
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
     }
 }
