8d26ffc09918b005db20f67c581ded54c35d9024
Re <refanopradana@gmail.com>
2020-12-22 08:22:31 +0700

    Add Layout

---
+++ app/src/main/java/com/kimi/tenki/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.kimi.tenki;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
