a47739a086484a1b78066922c830818063e97353
Re <refanopradana@gmail.com>
2020-12-22 19:30:09 +0700

    Add Layout

---
+++ app/src/main/java/com/kimi/tenki/SingleWeatherActivity.java
@@ -0,0 +1,14 @@
+package com.kimi.tenki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_single);
+    }
+}
